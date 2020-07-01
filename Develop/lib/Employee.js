4363d91683b54e02af667d33b83db4a448c34ca2
jdaeumler <jenniferdaeumler@gmail.com>
2020-06-30 19:27:46 -0500

    One test passed

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,1 +1,16 @@
 // TODO: Write code to define and export the Employee class
+// TODO: Write code to define and export the Employee class
+class Employee {
+    constructor (){
+        this.name = name;
+        // this.id = id;
+        // this.email = email;
+    }
+    // getName() 
+    // getId()
+    // getEmail()
+    // getRole() //returns 'Employee'
+}
+const e = new Employee();
+console.log(e);
+module.exports = Employee;
