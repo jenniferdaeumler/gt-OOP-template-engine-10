3bd1c188c0c4907631696ad6c110bba49c7b02b3
jdaeumler <jenniferdaeumler@gmail.com>
2020-07-01 18:32:36 -0500

    engineer test passed

--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -2,11 +2,16 @@
 const Employee = require("./Employee");
 
 class Engineer extends Employee {
-  constructor(name, id, email) {
+  constructor(name, id, email, github) {
     super(name, id, email);
     this.github = github;
   }
-  getGitHub(){
-      return this.github;
+  getGithub() {
+    return this.github;
+  }
+  getRole() {
+    return "Engineer";
   }
 }
+
+module.exports = Engineer;
