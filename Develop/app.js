01873ea9880fb455078ae09c919699a0f58901be
jdaeumler <jenniferdaeumler@gmail.com>
2020-07-01 19:02:04 -0500

    add another employee option added

--- Develop/app.js
+++ Develop/app.js
@@ -13,47 +13,56 @@ const render = require("./lib/htmlRenderer");
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 const questions = [
-  {
-    type: "choices",
-    message: "What is your role?",
-    name: "role",
-    choices: ["Engineer", "Intern", "Manager"],
-  },
-  {
-    type: "input",
-    message: "What is your name?",
-    name: "name",
-  },
-  {
-    type: "input",
-    message: "What is your ID?",
-    name: "id",
-  },
-  {
-    type: "input",
-    message: "What is your email?",
-    name: "email",
-  },
-  {
-    type: "input",
-    message: "What is your GitHub username?",
-    name: "github",
-    when: (response) => response.role === "Engineer",
-  },
-  {
-    type: "input",
-    message: "What school did you attend?",
-    name: "school",
-    when: (response) => response.role === "Intern",
-  },
-  {
-    type: "input",
-    message: "What is your office number?",
-    name: "officeNumber",
-    when: (response) => response.role === "Manager",
-  },
+    {
+        type: "list",
+        message: "What is your role?",
+        name: "role",
+        choices: ["Engineer", "Intern", "Manager"],
+    },
+    {
+        type: "input",
+        message: "What is your name?",
+        name: "name",
+    },
+    {
+        type: "input",
+        message: "What is your ID?",
+        name: "id",
+    },
+    {
+        type: "input",
+        message: "What is your email?",
+        name: "email",
+    },
+    {
+        type: "input",
+        message: "What is your GitHub username?",
+        name: "github",
+        when: (response) => response.role === "Engineer",
+    },
+    {
+        type: "input",
+        message: "What school did you attend?",
+        name: "school",
+        when: (response) => response.role === "Intern",
+    },
+    {
+        type: "input",
+        message: "What is your office number?",
+        name: "officeNumber",
+        when: (response) => response.role === "Manager",
+    },
+    {
+        type: "confirm",
+        message: "Do you want to add another employee?",
+        name: "add",
+      },
 ];
 
+inquirer.prompt(questions)
+.then(function(response){
+console.log(response);
+})
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
